fa2389ee6d45bd20c2913f28afe908dc3ce87fd3
tianze0926 <i@tianze.me>
2023-11-28 23:07:05 +0800

    fix tmux version

--- pkgs/tmux/default.nix
+++ pkgs/tmux/default.nix
@@ -1,15 +1,16 @@
 # https://github.com/NixOS/nixpkgs/issues/268472#issuecomment-1817820348
 { tmux
 , fetchFromGitHub
-}: tmux.overrideAttrs (x: {
-  configureFlags = (x.configureFlags or []) ++ [
-    "--enable-sixel"
-  ];
+}: tmux.overrideAttrs (x: rec {
+  version = "bdf8e614af34ba1eaa8243d3a818c8546cb21812";
   src = fetchFromGitHub {
     owner = "tmux";
     repo = "tmux";
-    rev = "bdf8e614af34ba1eaa8243d3a818c8546cb21812";
+    rev = version;
     hash = "sha256-ZMlpSOmZTykJPR/eqeJ1wr1sCvgj6UwfAXdpavy4hvQ=";
   };
   patches = [];
+  configureFlags = (x.configureFlags or []) ++ [
+    "--enable-sixel"
+  ];
 })
